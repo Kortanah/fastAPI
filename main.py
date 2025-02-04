4d4ba01639fa59e54268eddd4c44d48cade71259
Kortanah <123585486+Kortanah@users.noreply.github.com>
2025-02-04 19:30:40 +0000

    Update main.py

--- main.py
+++ main.py
@@ -24,12 +24,18 @@ app.add_middleware(
 app.mount("/runs", StaticFiles(directory="runs"), name="runs")
 
 # yolov5_path = os.path.abspath("./yolov5")
-model_path = os.path.abspath("./model/best.pt")
 
-# # Load model
+# Get the directory of the current file
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# Dynamic paths to the YOLOv5 directory and model file
+YOLO_DIR = os.path.join(BASE_DIR, "yolov5")
+MODEL_PATH = os.path.join(BASE_DIR, "best.pt")
+
+model = torch.hub.load(YOLO_DIR, 'custom', path=MODEL_PATH, source='local', device='cpu')
 
 # # Load the YOLOv5 model
-model = torch.hub.load("./yolov5", 'custom', path=model_path, source='local', device='cpu')
+# model = torch.hub.load("./yolov5", 'custom', path=model_path, source='local', device='cpu')
 
 # Load the YOLOv5 model
 #model = torch.hub.load('./yolov5', 'custom', path='./model/best.pt', source='local', device='cpu')
